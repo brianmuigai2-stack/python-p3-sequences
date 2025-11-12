d82b0d9da949e88629d8bb91b3db9cc1d468b3bb
brianmuigai2-stack <brian.muigai2@student.moringaschool.com>
2025-11-12 18:11:43 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  
+        return
+
+    result = []
+    a, b = 0, 1
+    for _ in range(length):
+        result.append(a)
+        a, b = b, a + b
+
+    print(result)
+
+    length = []
